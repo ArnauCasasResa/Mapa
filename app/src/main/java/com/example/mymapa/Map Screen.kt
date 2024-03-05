977922b348901587e0e3f124d197ba769f6c628a
arnau <arnau.casas.7e7@itb.cat>
2024-03-05 13:57:55 +0100

    paketa

--- app/src/main/java/com/example/mymapa/Map Screen.kt
+++ app/src/main/java/com/example/mymapa/Map Screen.kt
@@ -1,9 +1,14 @@
 package com.example.mymapa
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -14,9 +19,10 @@ import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
+@SuppressLint("MutableCollectionMutableState")
 @Composable
 fun MapScreen(navController: NavController){
-    var marcadores= mutableListOf<LatLng>()
+    var marcadores by remember { mutableStateOf(mutableListOf<LatLng>())}
    Column(modifier = Modifier
        .fillMaxSize()
        .padding(16.dp)) {
@@ -27,8 +33,7 @@ fun MapScreen(navController: NavController){
        }
        GoogleMap(modifier = Modifier.fillMaxSize(),cameraPositionState=cameraPositionState,
            onMapClick = {
-               val marcador=LatLng(0.0,0.0)
-               marcadores.add(marcador)
+               marcadores.add(it)
            }){
            for(i in marcadores){
                Marker(state = MarkerState(position = i),
